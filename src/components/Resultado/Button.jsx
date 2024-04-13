8179cbb821645ea173c450d2580dc696733dc2af
GlendyT <glendytuyuc@gmail.com>
2024-04-12 21:31:01 -0600

    responsive

--- src/components/Resultado/Button.jsx
+++ src/components/Resultado/Button.jsx
@@ -1,23 +1,21 @@
 
 
-const Button = ({ htmlToImageConvert,resetPhotos, changeColor}) => {
+const Button = ({ htmlToImageConvert,resetPhotos}) => {
 
   const handleDownload = () => {
-
     htmlToImageConvert();
   };
   return (
-    <div className="flex flex-row gap-2">
+    <div className="max-md:flex ">
       <button
-        className="bg-purple-800 rounded-lg text-white font-bold p-2 mt-4  disabled:bg-opacity-25 disabled:cursor-not-allowed transition-colors max-sm:text-xs max-sm:mt-2"
+        className="bg-purple-800 rounded-lg text-white font-bold p-2 mt-4 mr-2 disabled:bg-opacity-25 disabled:cursor-not-allowed transition-colors max-sm:text-xs max-sm:mt-2"
         onClick={handleDownload}
-
       >
         Save Photobooth
       </button>
 
       <button
-        className="bg-purple-800 rounded-lg text-white font-bold p-2 mt-4  disabled:bg-opacity-25 disabled:cursor-not-allowed transition-colors max-sm:text-xs max-sm:mt-2"
+        className="bg-purple-800 rounded-lg text-white font-bold p-2 mt-4 ml-2 disabled:bg-opacity-25 disabled:cursor-not-allowed transition-colors max-sm:text-xs max-sm:mt-2"
         onClick={resetPhotos}
 
       >
