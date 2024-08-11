a6da4d63821518c239f2027602bff828ef863f96
GlendyT <glendytuyuc@gmail.com>
2024-08-11 15:28:23 -0600

    actualizar logo suga y hobi

--- src/views/App.js
+++ src/views/App.js
@@ -18,41 +18,36 @@ useEffect(() => {
     <>
       (
       <div className=" relative min-h-screen bg-backmain  bg-cover bg-no-repeat max-sm:bg-center ">
-        <div className="grid grid-cols-4 px-40 py-32 gap-6 max-sm:flex max-sm:flex-col max-sm:p-12 max-sm:items-center max-sm:justify-center max-sm:gap-2">
+        <div className="grid grid-cols-4 px-40 py-32 gap-6 max-sm:flex max-sm:flex-col max-sm:p-12  max-sm:gap-3">
           <Link
             to="/polaroid"
-            className="px-4 py-16 bg-polaroidlogo bg-no-repeat bg-contain hover:bg-btsgif3 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
+            className="px-4 py-16 bg-polaroidlogo bg-no-repeat bg-contain bg-center hover:bg-btsgif3 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
           ></Link>
           <Link
             to="/photobooth"
-            className="px-4 py-16 bg-photostrips bg-no-repeat bg-contain hover:bg-btsgif1 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20  max-sm:bg-contain drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
+            className="px-4 py-16 bg-photostrips bg-no-repeat bg-contain bg-center hover:bg-btsgif1 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20  max-sm:bg-contain drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
           ></Link>
           <Link
             to="/sugaverse"
-            className=" bg-sugalogoi bg-no-repeat bg-contain px-10 py-16 hover:bg-backsuga hover:bg-cover hover:bg-center hover:bg-no-repeat  hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)] "
+            className=" bg-sugalogoi4 bg-no-repeat bg-contain bg-center px-10 py-16 hover:bg-backsuga hover:bg-cover hover:bg-center hover:bg-no-repeat  hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)] "
           ></Link>
           <Link
             to="/hobipalooza"
-            className=" bg-jhopelogoi bg-no-repeat bg-contain px-10 py-16 hover:bg-gifhobi hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
+            className=" bg-jhopelogoi bg-no-repeat bg-contain bg-center px-10 py-16 hover:bg-gifhobi hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
           ></Link>
 
           <Link
             to="/bingo"
-            className="px-4  py-16 bg-bingologoi bg-no-repeat bg-contain  hover:bg-btsgif2 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
+            className="px-4  py-16 bg-bingologoi bg-no-repeat bg-contain bg-center  hover:bg-btsgif2 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
           ></Link>
-
-          {/*       <Link to="/rain" className="px-4 max-sm:py-6 py-16 bg-black  hover:bg-btsgif2 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl  ">
-          <div className=" text-white  ">
-            <div className=" flex flex-row-2 gap-8 justify-center items-center max-sm:flex-col max-sm:text-center">
-              <div className="flex flex-col gap-8">
-                <div className="text-sm">
-                  BTS{" "}
-                  <h1 className="uppercase font-extrabold">Rain of Photos</h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Link>*/}
+          <Link
+            to="/vpassport"
+            className="px-4 py-16 bg-vlogo bg-no-repeat bg-contain bg-center hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
+          ></Link>
+          <Link
+            to="/hobisback"
+            className="px-4  py-16 bg-jhopelogoi1 bg-no-repeat bg-contain bg-center hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
+          ></Link>
         </div>
       </div>
     </>
