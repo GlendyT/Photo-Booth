23aacf28e5bcc3d8b83a0f7761252f38d4b09530
GlendyT <glendytuyuc@gmail.com>
2024-09-22 21:39:39 -0600

    styles for navbar and transitions for grid options

--- src/components/VPassport/VCitizenPage1.jsx
+++ src/components/VPassport/VCitizenPage1.jsx
@@ -1,6 +1,6 @@
 export default function VCitizenPage1({ image }) {
     return (
-      <div className=" shadow-md rounded-xl w-72 h-50  max-sm:w-44  ">
+      <div className=" shadow-md rounded-xl w-72 h-50    ">
         <img src={image} alt="" />
       </div>
     );
--- src/views/App.js
+++ src/views/App.js
@@ -20,33 +20,32 @@ useEffect(() => {
         <div className="grid grid-cols-4 px-40 py-32 gap-6 max-sm:flex max-sm:flex-col max-sm:p-12  max-sm:gap-3">
           <Link
             to="/polaroid"
-            className="px-4 py-16 bg-polaroidlogo bg-no-repeat bg-contain bg-center hover:bg-btsgif3 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
+            className=" delay-150 hover:-translate-y-1 hover:scale-100 duration-700 px-4 py-16 bg-polaroidlogo bg-no-repeat bg-contain bg-center hover:bg-btsgif3 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
           ></Link>
           <Link
             to="/photobooth"
-            className="px-4 py-16 bg-photostrips bg-no-repeat bg-contain bg-center hover:bg-btsgif1 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20  max-sm:bg-contain drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
+            className="delay-150 hover:-translate-y-1 hover:scale-100 duration-700 px-4 py-16 bg-photostrips bg-no-repeat bg-contain bg-center hover:bg-btsgif1 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20  max-sm:bg-contain drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
           ></Link>
           <Link
             to="/sugaverse"
-            className=" bg-sugalogoi4 bg-no-repeat bg-contain bg-center px-10 py-16 hover:bg-backsuga hover:bg-cover hover:bg-center hover:bg-no-repeat  hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)] "
+            className="delay-150 hover:-translate-y-1 hover:scale-100 duration-700 bg-sugalogoi4 bg-no-repeat bg-contain bg-center px-10 py-16 hover:bg-backsuga hover:bg-cover hover:bg-center hover:bg-no-repeat  hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)] "
           ></Link>
           <Link
             to="/hobipalooza"
-            className=" bg-jhopelogoi bg-no-repeat bg-contain bg-center px-10 py-16 hover:bg-gifhobi hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
+            className="delay-150 hover:-translate-y-1 hover:scale-100 duration-700 bg-jhopelogoi bg-no-repeat bg-contain bg-center px-10 py-16 hover:bg-gifhobi hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]"
           ></Link>
 
           <Link
             to="/bingo"
-            className="px-4  py-16 bg-bingologoi bg-no-repeat bg-contain bg-center  hover:bg-btsgif2 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
+            className=" delay-150 hover:-translate-y-1 hover:scale-100 duration-700 px-4  py-16 bg-bingologoi bg-no-repeat bg-contain bg-center  hover:bg-btsgif2 hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
           ></Link>
           <Link
             to="/vpassport"
-            className="px-4 py-16 bg-vlogo bg-no-repeat bg-contain bg-center  hover:bg-gifV hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
-
+            className="delay-150 hover:-translate-y-1 hover:scale-100 duration-700 px-4 py-16 bg-vlogo bg-no-repeat bg-contain bg-center  hover:bg-gifV hover:bg-cover hover:bg-center hover:bg-no-repeat hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
           ></Link>
           <Link
             to="/hobisback"
-            className="px-4  py-16 bg-jhopelogoi1 bg-no-repeat bg-contain bg-center hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
+            className=" delay-150 hover:-translate-y-1 hover:scale-100 duration-700 px-4  py-16 bg-jhopelogoi1 bg-no-repeat bg-contain bg-center hover:outline-4 hover:outline hover:outline-white hover:rounded-3xl max-sm:px-20 drop-shadow-[0_35px_35px_rgba(0,0,0,2)]"
           ></Link>
         </div>
       </div>
