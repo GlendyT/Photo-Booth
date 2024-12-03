ee77e2f0d1a2094ea58f8444decc4750a2050ceb
Glendy Tuyuc <glendytuyuc@gmail.com>
2024-12-02 20:34:56 -0600

    changes almost overdone7

--- src/components/sugaverse/Titulo/Header.jsx
+++ src/components/sugaverse/Titulo/Header.jsx
@@ -2,12 +2,12 @@
 
 export default function Header() {
     return (
-        <div className="block mt-1 text-4xl text-center font-bold text-white font-dmmono max-md:text-2xl">
+        <div className=" text-4xl text-center font-bold text-white font-dmmono max-md:text-2xl">
           Into the 
           <div
-           className="uppercase font-glich font-thin text-5xl max-md:text-4xl "
+           className="uppercase font-glich font-thin text-5xl  "
           >suga-verse</div>
-          <h1 className="text-center text-sm font-dmmono pb-5 max-md:text-1xl">
+          <h1 className="text-center text-sm font-dmmono pb-5 ">
             Create and share your lyrics inspired by Yoongi´s extraordinary
             lyricism
           </h1>
