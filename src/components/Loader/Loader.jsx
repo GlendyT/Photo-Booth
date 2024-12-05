8ad774ef9b3c36a990fc1fd2795706773280fc6f
Glendy Tuyuc <glendytuyuc@gmail.com>
2024-12-04 20:35:15 -0600

    test1

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,37 +1,11 @@
 import React from "react";
-import "./Loader.css";
 
 const Loader = () => {
   return (
-    <>
-      <span class="loader">
-        <div className="fixed right-5 bottom-5 flex items-center justify-center"></div>
-      </span>
-    </>
+    <div className="fixed right-5 bottom-5 flex items-center justify-center">
+      <span className="loader"></span>
+    </div>
   );
 };
 
 export default Loader;
-
-/*   
- camera
-<div id="camera">
-  <div className="strip">
-  </div>
-  <div className="lens">
-  </div>
-  <div className="led">
-  </div>
-  <div className="btn">
-  </div>
-</div>
-<div id="panel">
-  <div className="pic">
-    <img src="http://lorempixel.com/150/150/people" alt='imag'/>
-  </div>
-  <center>
-    <a className="lnk">My Portfolio</a>
-  </center>
-</div>
-<div className="shadow">
-</div>*/
