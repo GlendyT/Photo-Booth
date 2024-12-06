2ee5f524217830b70475195561865f935facda13
Glendy Tuyuc <glendytuyuc@gmail.com>
2024-12-06 08:29:00 -0600

    changes test12

--- src/views/Fishing.jsx
+++ src/views/Fishing.jsx
@@ -9,33 +9,29 @@ const Fishing = () => {
   const { showModal } = useFish();
   return (
     <div className="bg-[#2b74ce] flex flex-col min-h-screen font-PressGame justify-center ">
-      <div className="">
-        <div className="items-center flex flex-col max-sm:mt-9 max-sm:mx-8 mx-44 ">
-          <iframe
-            title="spotify"
-            style={{
-              borderRadius: "12px",
-            }}
-            src="https://open.spotify.com/embed/playlist/37i9dQZF1DX9ROcoR0cTZM?utm_source=generator&theme=0"
-            width="100%"
-            height="152"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-            allowtransparency="true"
-            loading="lazy"
-          ></iframe>
-
-          <p className="pb-2 text-lg font-extrabold text-center max-sm:text-xs">
-            While you play, do you want to listen to some music?
-          </p>
-        </div>
-
-        <div className="flex flex-col gap-6 items-center  ">
-          <HangmanDrawing />
-          <HangmanWord />
-          <Keyboard />
-        </div>
-        <Waves />
+      <div className="items-center flex max-sm:mt-8 max-sm:mx-10 mx-44 ">
+        <iframe
+          title="spotify"
+          style={{
+            borderRadius: "12px",
+          }}
+          src="https://open.spotify.com/embed/playlist/37i9dQZF1DX9ROcoR0cTZM?utm_source=generator&theme=0"
+          width="100%"
+          height="152"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          allowtransparency="true"
+          loading="lazy"
+        ></iframe>
       </div>
+      <p className="pb-2 text-lg font-extrabold text-center max-sm:text-xs">
+        While you play, do you want to listen to some music?
+      </p>
+      <div className="flex flex-col gap-6 items-center  ">
+        <HangmanDrawing />
+        <HangmanWord />
+        <Keyboard />
+      </div>
+      <Waves />
 
       {showModal ? <JinFormulario /> : null}
     </div>
