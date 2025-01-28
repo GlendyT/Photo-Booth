2f6cef9a3fc06d096a00a5ae1d92591dec901c0e
DaniM0613 <wwh.rj.92@gmail.com>
2025-01-28 00:04:43 -0600

    Modificando el codigo

--- src/components/ValentinDay/Formulario.jsx
+++ src/components/ValentinDay/Formulario.jsx
@@ -1,20 +1,18 @@
-
-import React, {useState } from 'react'
-import { membersBts } from './data';
-import useDownload from '../../hooks/useDownload';
-import Card from './Card';
+import useRequestInfo from '../../hooks/useRequestInfo';
+import ListV from './ListV';
 
 
 export default function Formulario() {
-  const [to, setTo] = useState("")
-  const [from, setFrom] = useState("")
-  const [selectedMembers, setSelectedMembers] = useState(null)
+  //const [to, setTo] = useState("")
+  //const [from, setFrom] = useState("")
+  /*const [selectedMembers, setSelectedMembers] = useState(null)
   const [cardData, setCardData] = useState(null)
-  const [showForm, setShowForm] = useState(true)
-  const { handleDownloadImage } = useDownload()
+  const [showForm, setShowForm] = useState(true)*/
+  const { usuario, handleSubmit, handleName, handleContent} = useRequestInfo()
+  const { name, content } = usuario
 
 
-  const handleSubmit = (e) => {
+  /*const handleSubmit = (e) => {
     e.preventDefault()
     if(selectedMembers !== null) {
       const selected = membersBts.find((members) => members.id === parseInt(selectedMembers) )
@@ -35,11 +33,10 @@ export default function Formulario() {
     setSelectedMembers(null)
     setCardData(null)
     setShowForm(true)
-  }
+  }*/
 
   return (
     <>
-    {showForm  ? ( 
       <div className="justify-center items-center flex fixed inset-10 z-40 ">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
           <div className="border border-gray-300 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none rounded-lg p-5">
@@ -54,28 +51,32 @@ export default function Formulario() {
               className="flex flex-col items-center justify-center"
             >
              <div className='mt-2 w-full'> 
-              <label htmlFor='to' className=' text-purple-700 font-bold mb-2'>To: </label>
+              <label htmlFor="name" className=' text-purple-700 font-bold mb-2'>To: </label>
               <input
+                id= "name"
                 type="text"
-                value={to}
-                onChange={(e) => setTo(e.target.value)}
+                name='name'
+                value={name}
+                onChange={handleName}
                 placeholder="Write the correct word"
                 className="text-center font-providence border border-purple-900 rounded-xl py-2 "
               />
              </div>
 
              <div className='mt-4 mb-5 w-full'> 
-              <label htmlFor='from' className=' text-purple-700 font-bold mb-2'>From: </label>
+              <label htmlFor="content" className=' text-purple-700 font-bold mb-2'>From: </label>
               <input
+                id="content"
                 type="text"
-                value={from}
-                onChange={(e) => setFrom(e.target.value)}
+                name='content'
+                value={content}
+                onChange={handleContent}
                 placeholder="Write the correct word"
                 className="text-center font-providence border border-purple-900 rounded-xl py-2 "
               />
              </div>
  
-             <div> 
+           { /* <div> 
               <label htmlFor='membersBts' className=' text-purple-700 font-bold mb-2'>
                  Make a choice :
               </label>
@@ -86,8 +87,11 @@ export default function Formulario() {
                <option key={members.id} value={members.id}> {members.name}</option>
                ) )}
              </select>
-             </div>
+             </div> */ }
 
+             <ListV
+                
+             />
             <div className='flex justify-center mt-5 items-center '>
               <button type="submit" className='text-center border border-purple-950 rounded-xl py-2 px-6 font-providence text-purple-800'>Create Card</button>
             </div>
@@ -96,19 +100,6 @@ export default function Formulario() {
           </div>    
         </div>
       </div>
-
-      ) : ( 
-        
-     <Card 
-       to={cardData.to}
-       from={cardData.from}
-       image={cardData.image}
-       onDownload={handleDownloadImage}
-       onRestart={handleRestart}
-      />
-  
-
-    )}
   
       <div className="opacity-30 fixed inset-10 z-30 bg-violet-900 "></div>
       
---
+++ src/components/ValentinDay/ListV.jsx
@@ -0,0 +1,47 @@
+
+import useRequestInfo from "../../hooks/useRequestInfo";
+import { membersBts } from "./data";
+
+
+export default function ListV() {
+
+  const { usuario, usuarioGenerado1 } =  useRequestInfo()
+  const { diseño } = usuario
+
+
+  return (
+    <div>
+      <div className="my-5">
+        <label className="text-sm mb-2 text-white">Design</label>
+
+        {/* <select
+          id="diseno"
+          name="diseño"
+          value={diseño}
+          placeholder="Select Your ArtWork"
+          onChange={usuarioGenerado}
+          className="shadow border rounded w-full py-2 px-3 text-gray-700  "
+        >
+        </select> */}
+
+        <div> 
+              <label htmlFor='membersBts' className=' text-purple-700 font-bold mb-2'>
+                 Make a choice :
+              </label>
+
+             <select
+              id="diseno"
+              name="diseño"
+              value={diseño} 
+              onChange={usuarioGenerado1}>
+               <option value="" className='text-2xl font-providence font-bold text-purple-800 border border-purple-900' >  Pick a member or the group  </option>
+               {membersBts.map((members) =>(
+                
+               <option key={members.id} value={members.id}> {members.name}  </option>
+               ) )}
+             </select>
+             </div>
+      </div>
+    </div>
+  );
+}
