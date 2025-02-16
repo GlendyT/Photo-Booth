3603d6d258c6fceda781b7af7430d0e065de1e27
Glendy Tuyuc <glendytuyuc@gmail.com>
2025-02-15 21:55:29 -0600

    changes rps2

--- src/components/ValentinDay/Card.jsx
+++ src/components/ValentinDay/Card.jsx
@@ -3,38 +3,28 @@ import useDownload from "../../hooks/useDownload";
 import { btsImg, membersBts } from "./data";
 import { useRef } from "react";
 
-
-
 export default function Card() {
   const { usuario, handleResetContent } = useRequestInfo();
   const { name, content, diseño } = usuario;
   const { handleDownloadImage } = useDownload();
 
-    const elementRef = useRef(null);
-
-
+  const elementRef = useRef(null);
 
   return (
     <div elementRef={elementRef}>
-
-      <div
-         id="print"
-         className= " flex flex-col w-full "
-      >
-                <>
-                     <div
-                       className={membersBts[diseño].div1}
-                       style={{ backgroundImage: `url(${btsImg[diseño]}) `}}
-                       id="print"
-                     >
-                       <div className={membersBts[diseño].div2}>
-                         <div className={membersBts[diseño].div3}>{name}</div>
-                         <p className={membersBts[diseño].p}> {content}</p>
-                       </div>
-                     </div>
-
-                   </>
-
+      <div id="print" className=" flex flex-col w-full ">
+        <>
+          <div
+            className={membersBts[diseño].div1}
+            style={{ backgroundImage: `url(${btsImg[diseño]}) ` }}
+            id="print"
+          >
+            <div className={membersBts[diseño].div2}>
+              <div className={membersBts[diseño].div3}>{name}</div>
+              <p className={membersBts[diseño].p}> {content}</p>
+            </div>
+          </div>
+        </>
       </div>
 
       <div className="flex justify-center mt-5 items-center space-x-4">
